053cf396fc1368cc05caa9bb757deddd4c8d1126
Atasha Frances Gayle Doria <asdoria@student.apc.edu.ph>
2025-09-23 19:54:41 +0800

    Fix server startup and routing issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { useAuthContext } from './contexts/AuthContext';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import DashboardPage from './pages/DashboardPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          {/* Additional routes will be added here */}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route 
+          path="/login" 
+          element={
+            <ProtectedRoute requireAuth={false}>
+              <LoginPage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/signup" 
+          element={
+            <ProtectedRoute requireAuth={false}>
+              <SignUpPage />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute requireAuth={true}>
+              <DashboardPage />
+            </ProtectedRoute>
+          } 
+        />
+      </Routes>
+    </div>
   );
 }
 
