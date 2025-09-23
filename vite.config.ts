8eb07bf555da3ca7eb77954eff8f7837cf31bd99
Atasha Frances Gayle Doria <asdoria@student.apc.edu.ph>
2025-09-23 19:36:40 +0800

    Fix crypto polyfill for Node.js 16

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
   },
+  server: {
+    host: true,
+    port: 5173
+  }
 })
